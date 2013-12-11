a0dc2f17855ff8b8ca4251ab6ffc06d7b35b7829
Christopher Gwilliams <encima@gmail.com>
2013-12-11 07:24:26 +0000

    beta release before demo

--- demo_player_tp.py
+++ demo_player_tp.py
@@ -2,10 +2,10 @@ import mcpi.minecraft as minecraft
 
 def tp_player(x,y,z):
 	#If getPos gets the position, what does setPos do?
+	print 'teleporting'
 	mc.player.setPos(x+20, y+40, z+20)
 
-if __name__ == "main":
-	mc = minecraft.Minecraft.create(address='localhost', port=4711)
-	x,y,z = mc.player.getTilePos()
-	#Call tp_player method
-	tp_player(p_loc)
+mc = minecraft.Minecraft.create(address='localhost', port=4711)
+x,y,z = mc.player.getPos()
+#Call tp_player method
+tp_player(x,y,z)
